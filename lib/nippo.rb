a5f0ec4739fc260776b49162514c33bc1fe7b62d
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2015-12-18 21:40:54 +0900

    add processing to load plug-in modules

--- lib/nippo.rb
+++ lib/nippo.rb
@@ -1,3 +1,12 @@
 require "nippo/version"
 require "nippo/server"
 require "nippo/task"
+
+module Nippo
+  # load plugins
+  plugin_path = File.dirname(__FILE__) + "/plugin"
+
+  Dir["#{plugin_path}/*"].each do |file|
+      require file
+  end
+end
